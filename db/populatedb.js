1866bee24c3590b9a6c1eb27fc14d1e236244ab8
Simon-Sahlin <simonsahlin@somin.se>
2024-08-18 00:23:13 +0200

    Fix faulty default db entry where author_id was 0

--- db/populatedb.js
+++ db/populatedb.js
@@ -22,8 +22,8 @@ CREATE TABLE IF NOT EXISTS messages (
 
 INSERT INTO messages (author_id, title, content, membership_req) 
 VALUES
-    (0, 'I love cats!', 'idk why they are just so awesome', 0),
-    (1, 'You cant see this!', 'Haha you need an extra level of membership to see this. If you do, youre very special', 1)
+    (1, 'I love cats!', 'idk why they are just so awesome', 0),
+    (2, 'You cant see this!', 'Haha you need an extra level of membership to see this. If you do, youre very special', 1)
 ;
 `;
 
