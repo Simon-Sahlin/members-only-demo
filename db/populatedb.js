f641bfbf12a0a9cdfbfefaa6a564e3f397025440
Simon-Sahlin <simonsahlin@somin.se>
2024-08-18 00:49:09 +0200

    Updated default db data and added admin user

--- db/populatedb.js
+++ db/populatedb.js
@@ -25,17 +25,24 @@ VALUES
     (1, 'I love cats!', 'idk why they are just so awesome', 0),
     (2, 'You cant see this!', 'Haha you need an extra level of membership to see this. If you do, youre very special', 1)
 ;
+
+INSERT INTO users (first_name, last_name, username, password_digest, membership_status, admin_status)
+VALUES
+  ('q', 'q', 'q', '$2a$10$gTpd5UPrKWHPwWjHidvB4OpX404fCh5T9Om1QNwx7ZH684sXpgdfq', 0, 0),
+  ('w', 'w', 'w', '$2a$10$exsrpyADOszpVuNmq1opjuGU0A21FZzkTJN.GcYoDpBtJN/AxfCAG', 1, 0),
+  ('Boss', 'Man', 'Admin', '$2a$10$MmlvtXOwYORtSXTW77/XZe6w/VgE.BLGmGRkIZUuojeJaoPxgp.VS', 1, 1)
+;
 `;
 
 async function main() {
-  console.log("Seeding database...");
-  const client = new Client({
-    connectionString: process.env.dbConnectionString,
-  });
-  await client.connect();
-  await client.query(SQL);
-  await client.end();
-  console.log("Done");
+    console.log("Seeding database...");
+    const client = new Client({
+      connectionString: process.env.dbConnectionString,
+    });
+    await client.connect();
+    await client.query(SQL);
+    await client.end();
+    console.log("Done");
 }
 
 // Connection string example:
